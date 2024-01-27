91533ee2bcbbd24a00680b945545728dbc8f40cf
Dominik Zalewski <78603984+doxenix@users.noreply.github.com>
2024-01-27 19:37:46 +0100

    Update repo.py

--- repo.py
+++ repo.py
@@ -1,3 +1,22 @@
-from feast import Entity
+from feast import Entity, BigQuerySource
+from feast.types import Float32, Float64, Int64
 
 driver = Entity(name="driver", join_keys=["driver_id"])
+
+my_bigquery_source = BigQuerySource(
+    table="fs-course-feast.feast.driver-stats",
+    timestamp_field="event_timestamp"
+)
+
+driver_stats_fv = FeatureView(
+    name="driver_hourly_stats_bq",
+    entities=[driver], 
+    schema=[
+        Field(name="conv_rate", dtype=Float32),
+        Field(name="acc_rate", dtype=Float32),
+        Field(name="avg_daily_trips", dtype=Int64, description="Average daily trips"),
+    ],
+    online=True,
+    source=my_bigquery_source,
+    tags={"team": "driver_performance"},
+)
